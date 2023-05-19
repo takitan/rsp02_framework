dde46827ff384bb6ff1333b00de637a25e6e1a8b
takitan <takitan1972@gmail.com>
2023-05-19 17:43:38 +0900

    Mission専用定義がいろいろ汚染していることが発覚したのでクビにしてる最中

--- src/MissionDefine.hpp
+++ src/MissionDefine.hpp
@@ -1,16 +1,16 @@
 #pragma once
-#include "rsp02.hpp"
+#include "rsp02_mission.hpp"
 #include "fw/command/CommandImplBase.hpp"
+/*
 #include "system/TLVDatalink.hpp"
 #include "system/TLVTransport.hpp"
 #include "system/CommandKernel.hpp"
 #include "system/SystemManager.hpp"
-
-using MissionTLV = TLVpacket_t<EDestination,EType>;
+*/
 using ICommand = rsp::rsp02::fw::command::ICommand<MissionTLV>;
-
 template<typename CMD_T, typename RES_T>
 using CommandImplBase =rsp::rsp02::fw::command::CommandImplBase<CMD_T,RES_T,MissionTLV>;
+/*
 
 using CommandKernel = rsp::rsp02::system::CommandKernel<MissionTLV,MissionTLV,MissionTLV>;
 //using TLVTransport = rsp::rsp02::system::TLVTransport<MissionTLV>;
@@ -18,3 +18,4 @@ using TLVDatalinkUp = rsp::rsp02::system::TLVDatalinkUp<MissionTLV>;
 using TLVDatalinkDown = rsp::rsp02::system::TLVDatalinkDown<MissionTLV>;
 
 using SystemManager = rsp::rsp02::system::SystemManager<MissionTLV>;
+*/
