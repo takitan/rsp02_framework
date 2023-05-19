de0e2cc1c4bc64670b76394d75508ea73e841adc
takitan <takitan1972@gmail.com>
2023-05-19 17:45:00 +0900

    実装を分離

---
+++ src/system/Shell.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "IShell.hpp"
+
+namespace rsp{
+namespace rsp02{
+namespace system{
+
+namespace detail
+{
+	class ShellImpl;
+}
+
+class Shell : public IShell
+{
+	friend class detail::ShellImpl;
+	public:
+		Shell();
+		void RegisterCommand( IShellCommand* cmd);
+		int Invoke( int argc, char** argv, void* extobj);
+	private:
+		detail::ShellImpl* impl;
+};
+
+}
+}
+}
