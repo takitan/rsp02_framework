e72c17a7c2e89faab03293ef4a6a2e899f485266
takitan <takitan1972@gmail.com>
2023-07-28 14:53:01 +0900

    コマンドを少しだけまじめに実装

--- src/RequestPingCommand.cpp
+++ src/RequestPingCommand.cpp
@@ -2,15 +2,16 @@
 #include "fw/logger/ILogger.hpp"
 
 TRequestPingCommand::TRequestPingCommand() :
-	CommandImplBase( "RequestPing", EDestination::Ground, EType::RequestPing),Response(buffer)
+	CommandImplBase( "RequestPing", EDestination::Mission, EType::RequestPing),Response(buffer)
 {}
 
-TRequestPingCommand::ExecuteStatus TRequestPingCommand::ConcreteExecute( const RequestPingCommand_t &cmd)
+TRequestPingCommand::ExecuteStatus TRequestPingCommand::ConcreteExecute( const RequestPingCommand_t& cmd)
 {
-	auto len = std::min<int>(cmd.Length, RequestPingResponse_t::PayloadSize);
-	Response = RequestPingResponse_t( buffer);
-	memcpy( &Response.pValue, &cmd.pValue, len);
+	Response.Destination( EDestination::Ground);
+	Response.Type( EType::ReplyPing);
+	Response.Length( cmd.Length());
+	auto len = std::min<int>(Response.Length(), RequestPingResponse_t::PayloadSize);
+	memcpy( Response.pValue(), cmd.pValue(), len);
 	SendRequest( Response);
-
 	return ExecuteStatus::Success;
 }
--- src/RequestTakePhotoCommand.cpp
+++ src/RequestTakePhotoCommand.cpp
@@ -1,10 +1,15 @@
 #include "RequestTakePhotoCommand.hpp"
 #include "fw/logger/ILogger.hpp"
 
-TRequestTakePhotoCommand::TRequestTakePhotoCommand():CommandImplBase( "RequestTakePhotoCommand", EDestination::Ground, EType::RequestTakePhoto){}
+TRequestTakePhotoCommand::TRequestTakePhotoCommand():
+	CommandImplBase( "RequestTakePhotoCommand", EDestination::Mission, EType::RequestTakePhoto),Response(buffer){}
 
 TRequestTakePhotoCommand::ExecuteStatus TRequestTakePhotoCommand::ConcreteExecute( const RequestTakePhotoCommand_t &cmd)
 {
+	(void)cmd;
+	Response.Destination(EDestination::Ground);
+	Response.Type(EType::ReplyTakePhoto);
+	Response.Length(0);
 	SendRequest( Response);
 
 	return ExecuteStatus::Success;
