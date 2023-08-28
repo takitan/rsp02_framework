63e9279d5c94c1958b579e79232b80f9d2fefba2
takitan <takitan1972@gmail.com>
2023-08-28 17:21:19 +0900

    ETypeを略称表記

--- src/RequestPingCommand.cpp
+++ src/RequestPingCommand.cpp
@@ -2,13 +2,13 @@
 #include "fw/logger/ILogger.hpp"
 
 TRequestPingCommand::TRequestPingCommand() :
-	CommandImplBase( "RequestPing", EDestination::Mission, EType::RequestPing),Response(buffer)
+	CommandImplBase( "RequestPing", EDestination::Mission, EType::ReqPing),Response(buffer)
 {}
 
 TRequestPingCommand::ExecuteStatus TRequestPingCommand::ConcreteExecute( const RequestPingCommand_t& cmd)
 {
 	Response.Destination( EDestination::Ground);
-	Response.Type( EType::ReplyPing);
+	Response.Type( EType::RepPing);
 	Response.Length( cmd.Length());
 	auto len = std::min<int>(Response.Length(), RequestPingResponse_t::PayloadSize);
 	memcpy( Response.pValue(), cmd.pValue(), len);
--- src/RequestTakePhotoCommand.cpp
+++ src/RequestTakePhotoCommand.cpp
@@ -2,13 +2,13 @@
 #include "fw/logger/ILogger.hpp"
 
 TRequestTakePhotoCommand::TRequestTakePhotoCommand():
-	CommandImplBase( "RequestTakePhotoCommand", EDestination::Mission, EType::RequestTakePhoto),Response(buffer){}
+	CommandImplBase( "RequestTakePhotoCommand", EDestination::Mission, EType::ReqTakePhoto),Response(buffer){}
 
 TRequestTakePhotoCommand::ExecuteStatus TRequestTakePhotoCommand::ConcreteExecute( const RequestTakePhotoCommand_t &cmd)
 {
 	(void)cmd;
 	Response.Destination(EDestination::Ground);
-	Response.Type(EType::ReplyTakePhoto);
+	Response.Type(EType::RepTakePhoto);
 	Response.Length(0);
 	SendRequest( Response);
 
