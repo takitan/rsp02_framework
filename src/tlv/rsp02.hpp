12c1ba41f29abdb3c41ac744dd55bd4bd129036e
takitan <takitan1972@gmail.com>
2024-04-22 21:26:13 +0900

    場所間違い

--- src/tlv/rsp02.hpp
+++ src/tlv/rsp02.hpp
@@ -1,68 +1,48 @@
+/**
+ * @file rsp02.hpp
+ * @author Nobuyuki Takita (takitan1972@gmail.com)
+ * @brief RSP02全系共通定義
+ * @version 0.1
+ * @date 2024-04-18
+ */
 #pragma once
 #include <cstdint>
 #include <cstring>
 #include <cstdio>
 #include "fw/util/align.hpp"
 
-/*
-struct TLVmessage_t
-{
-	constexpr static std::size_t pValueSize = 256;
-	typedef uint8_t type_t;
-	typedef uint8_t dst_t;
-	typedef uint16_t len_t;
-
-	dst_t destination;
-	type_t type;
-	len_t length;
-	uint8_t pValue[pValueSize];
-	TLVmessage_t():destination(0),type(0),length(0)
-	{
-		memset(pValue, 0, pValueSize);
-	}
-};
-*/
-
 enum class EDestination : uint8_t
 {
-	Null		= 0,
-	Ground		= 1,
-	Rx			= 2,
-	Tx1			= 3,
-	Tx2			= 4,
-	CDH			= 5,
-	Mission		= 6,
-	ParentLora	= 7,
-	Child1		= 8,
-	Child2		= 9,
-	NumOfDestination,
+	Null				= 0,	//< 未定義
+	Ground				= 1,	//< 地上局
+	Rx					= 2,	//!< 受信機
+	Tx1					= 3,	//!< 主送信機
+	Tx2					= 4,	//!< 副送信機
+	CDH					= 5,	//!< C&DH系
+	Mission				= 6,	//!< Mission系
+	ParentLora			= 7,	//!< 親局LoRa
+	Child1				= 8,	//!< 小衛星1
+	Child2				= 9,	//!< 小衛星2
+	NumOfDestination,			//!< 番兵くん
 };
 
-enum class ECMDType : uint8_t
+static inline const char* DestinationString(EDestination dst)
 {
-	Null			= 0x00,
-	COMM_TEST		= 0x01,
-	GET_HKDATA		= 0x11,
-	RESET_MAINOBC	= 0x12,
-	EXPAND_ANTENNA	= 0x13,
-	SET_FDIR		= 0x16,
-	POWER			= 0x20,
-	GET_LEVEL		= 0x74,
-	CHECK_FCS		= 0x77,
-	CHECK_DEST		= 0x78,
-	TX_TEST			= 0x79,
-	RX_SW_RESET		= 0x7a,
-	SEND_CW			= 0x81,
-	SEND_GMSK		= 0x85,
-	ABORT_DOWNLINK	= 0x86,
-	SET_CW_SPEED	= 0x87,
-	SELECT_TXOBC	= 0x91,
-	TX_RESET		= 0x92,
-	CHANGE_TX_MODE	= 0x93,
-	TLVMSG			= 0xe1,
-	TIME_SYNC		= 0xe2,
-	NumOfCMDType,
-};
+	switch( dst)
+	{
+		case EDestination::Null			: return "Null!";
+		case EDestination::Ground		: return "Ground";
+		case EDestination::Rx			: return "Rx";
+		case EDestination::Tx1			: return "Tx1";
+		case EDestination::Tx2			: return "Tx2";
+		case EDestination::CDH			: return "CDH";
+		case EDestination::Mission		: return "Mission";
+		case EDestination::ParentLora	: return "ParentLora";
+		case EDestination::Child1		: return "Child1";
+		case EDestination::Child2		: return "Child2";
+		default: return "";
+	}
+}
 
 template<typename T>
 struct SafeProperty
@@ -79,24 +59,6 @@ struct SafeProperty
 		T operator()() const
 		{
 			return align::safe_read<T>( (uint8_t*)origin);
-		}
-};
-
-template<typename T>
-struct SafePropertyEx
-{
-	using align = rsp::rsp02::fw::util::align;
-	private:
-		T value;
-	public:
-		SafePropertyEx(){}
-		void operator()(T src)
-		{
-			value = src;
-		}
-		T operator()() const
-		{
-			return value;
 		}
 };
 
@@ -133,106 +95,8 @@ struct PacketBase_t
 		Original = dst.Original;
 		sender_id = dst.sender_id;
 	}
-
-	protected:
-		inline const void* org_pnt(size_t ofs){ return (uint8_t*)Original+ofs;}
-		inline const void* pv_pnt(uint8_t* org, size_t ofs){ return org+ofs;}
 };
 
-/**
- * @brief 地上局がC系に送信するコマンド
- * @tparam TYP_T コマンドタイプの型
- */
-template<typename TYP_T>
-struct CMDpacket_t : PacketBase_t
-{
-	using typ_t = TYP_T;
-	constexpr static size_t typ_offset = 0;
-	constexpr static size_t par_offset = 1;
-
-	CMDpacket_t() : CMDpacket_t((const void*)nullptr, 0){}
-	CMDpacket_t(const void* org, uint16_t len) : CMDpacket_t(org,len, -1){}
-	CMDpacket_t(const void* org, uint16_t len, int sender) :
-		PacketBase_t(org,sender),
-		Type(org_pnt(typ_offset)),
-		Length(len),
-		pParam(org_pnt(par_offset)){}
-	// コマンドのタイプ	
-	SafeProperty<typ_t> Type;
-	SafePropertyEx<uint16_t> Length;
-	// コマンドパラメータ(可変長)
-	SafeArray<uint8_t> pParam;
-
-	/**
-	 * @brief パケットを表示用に整形する
-	 * 
-	 * @param buf 整形済テキストの格納バッファ
-	 * @pre bufには、必要十分なサイズを用意しておくこと
-	 */
-	void print(char* buf)
-	{
-		int p = ::sprintf( buf, "dst:%02x(%s),type:%02x,length:%04x,pv:",
-			(uint8_t)Destination(), DestinationString(Destination()),
-			(uint8_t)Type(), Length());
-		len_t len = Length();
-		auto pvpnt = pValue();
-		for(int i=0;i<len;i++)
-		{
-			uint8_t pv = pvpnt[i];
-			p += ::sprintf( &buf[p], "%02x", pv);
-		}
-	}
-};
-
-/**
- * @brief C系間から地上局に返すレスポンス
- * @tparam TYP_T コマンドタイプの型
- */
-template<typename TYP_T>
-struct RESpacket_t : PacketBase_t
-{
-	using typ_t = TYP_T;
-	constexpr static size_t typ_offset = 0;
-	constexpr static size_t par_offset = 1;
-
-	RESpacket_t() : RESpacket_t((const void*)nullptr){}
-	RESpacket_t(const void* org) : RESpacket_t(org,-1){}
-	RESpacket_t(const void* org, int sender) :
-		PacketBase_t(org,sender),
-		Type(org_pnt(typ_offset)),
-		pParam(org_pnt(par_offset)){}
-	// コマンドのタイプ	
-	SafeProperty<typ_t> Type;
-	// コマンドパラメータ(可変長)
-	SafeProperty<uint8_t> Param;
-
-	/**
-	 * @brief パケットを表示用に整形する
-	 * 
-	 * @param buf 整形済テキストの格納バッファ
-	 * @pre bufには、必要十分なサイズを用意しておくこと
-	 */
-	void print(char* buf)
-	{
-		int p = ::sprintf( buf, "type:%02x,length:%04x,pv:",
-			(uint8_t)Destination(), DestinationString(Destination()),
-			(uint8_t)Type(), Length());
-		len_t len = Length();
-		auto pvpnt = pValue();
-		for(int i=0;i<len;i++)
-		{
-			uint8_t pv = pvpnt[i];
-			p += ::sprintf( &buf[p], "%02x", pv);
-		}
-	}
-};
-
-/**
- * @brief TLVコマンドレスポンス
- * 
- * @tparam DST_T Destinationの型
- * @tparam TYP_T コマンドタイプの型
- */
 template<typename DST_T,typename TYP_T>
 struct TLVpacket_t : PacketBase_t
 {
@@ -260,12 +124,6 @@ struct TLVpacket_t : PacketBase_t
 	SafeProperty<len_t> Length;
 	SafeArray<uint8_t> pValue;
 
-	/**
-	 * @brief パケットを表示用に整形する
-	 * 
-	 * @param buf 整形済テキストの格納バッファ
-	 * @pre bufには、必要十分なサイズを用意しておくこと
-	 */
 	void print(char* buf)
 	{
 		int p = ::sprintf( buf, "dst:%02x(%s),type:%02x,length:%04x,pv:",
@@ -279,24 +137,11 @@ struct TLVpacket_t : PacketBase_t
 			p += ::sprintf( &buf[p], "%02x", pv);
 		}
 	}
+
+	protected:
+		inline const void* org_pnt(size_t ofs){ return (uint8_t*)Original+ofs;}
+		inline const void* pv_pnt(size_t ofs){ return (uint8_t*)org_pnt(pv_offset)+ofs;}
 };
 
 using rsp02TLV = TLVpacket_t<EDestination,uint8_t>;
 
-static inline const char* DestinationString(EDestination dst)
-{
-	switch( dst)
-	{
-		case EDestination::Null: return "Null!";
-		case EDestination::Ground: return "Ground";
-		case EDestination::Rx: return "Rx";
-		case EDestination::Tx1: return "Tx1";
-		case EDestination::Tx2: return "Tx2";
-		case EDestination::CDH: return "CDH";
-		case EDestination::Mission: return "Mission";
-		case EDestination::ParentLora: return "ParentLora";
-		case EDestination::Child1: return "Child1";
-		case EDestination::Child2: return "Child2";
-		default: return "";
-	}
-}
--- src/tlv/rsp02_cdh.hpp
+++ src/tlv/rsp02_cdh.hpp
@@ -1,30 +1,109 @@
 #pragma once
+/**
+ * @file rsp02_cdh.hpp
+ * @author Nobuyuki Takita (takitan1972@gmail.com)
+ * @brief C&DH系定義
+ * @version 0.1
+ * @date 2024-04-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
 #include "rsp02.hpp"
 #include "fw/command/CommandTypeBase.hpp"
 
-enum class EType : uint8_t
+/** @brief StateID */
+enum class StateID : int
 {
-	ReqNull = 0x00,
-	ReqPing = 0x01,
-	ReqTakePhoto = 0x02,
-	RepNull = 0x80,
-	RepPing = 0x81,
-	RepTakePhoto = 0x82,
+	Idle		= 0,	//<! Idle
+	State1		= 1,	//<! State1
+	State2		= 2,	//!< State2
 };
 
-using TLVPacket = TLVpacket_t<EDestination, EType>;
+/** @brief TLVコマンド */
+enum class EType : uint8_t
+{
+	ReqNull				= 0,	//!< 未定義
+	RESET_MAINOBC,				//!< 衛星全体リセット要求
+	RESET_MAINOBC_RES,			//!< 衛星全体リセット応答
+	EXPAND_ANTENNA,				//!< アンテナ展開要求
+	EXPAND_ANTENNA_RES,			//!< アンテナ展開応答
+	SET_FDIR,					//!< 異常処置設定要求
+	SET_FDIR_RES,				//!< 異常処置設定応答
+	POWER,						//!< 搭載機器電源設定要求
+	POWER_RES,					//!< 搭載機器電源設定応答
+	SEND_CW,					//!< CW送信要求
+	SEND_CW_RES,				//!< CW送信応答
+	SEND_GMSK,					//!< GMSK送信要求
+	SEND_GMSD_RES,				//!< GMSK送信応答
+	ABORT_DOWNLINK,				//!< 送信キャンセル要求
+	ABORT_DOWNLINK_RES,			//!< 送信キャンセル応答
+	SET_CW_SPEED,				//!< CW送信速度設定要求
+	SET_CW_SPEED_RES,			//!< CW送信速度設定応答
+	SELECT_TXOBC,				//!< 送信機切替要求
+	SELECT_TXOBC_RES,			//!< 送信機切替応答
+	TX_RESET,					//!< 送信機リセット要求
+	TX_RESET_RES,				//!< 送信機リセット応答
+	COMM_TEST,					//!< 衛星内通信テスト要求
+	COMM_TEST_RES,				//!< 衛星内通信テスト応答
+	GET_HKDATA_FULL,			//!< 全系HKデータ要求
+	GET_HKDATA_FULL_RES,		//!< 全系HKデータ応答
+	SET_CUR_TIME		= 100,	//!< 自系現在時刻設定要求
+	SET_CUR_TIME_RES,			//!< 自系現在時刻設定応答
+	GET_HKDATA,					//!< 自系HKデータ要求
+	GET_HKDATA_RES,				//!< 自系HKデータ応答
+	REQ_PING,					//!< 自系死活確認要求
+	REQ_PING_RES,				//!< 自系死活確認応答
+	REQ_TEMPERATURE,			//!< 自系温度要求
+	REQ_TEMPERATURE_RES,		//!< 自系温度応答
+};
 
+using CdhTLV = TLVpacket_t<EDestination, EType>;
+
+/**
+ * @brief StateIDから文字列を得る
+ * 
+ * @param dst StateID
+ * @return const char* 対応する文字列
+ */
 static inline const char* TypeString(EType dst)
 {
 	switch( dst)
 	{
-		case EType::ReqNull: return "ReqNull!";
-		case EType::ReqPing: return "ReqPing";
-		case EType::ReqTakePhoto: return "ReqTakePhoto";
-		case EType::RepNull: return "RepNull";
-		case EType::RepPing: return "RepPing";
-		case EType::RepTakePhoto: return "RepTakePhoto";
+		case EType::ReqNull:				return "ReqNull";
+		case EType::RESET_MAINOBC:			return "RESET_MAINOBC";
+		case EType::RESET_MAINOBC_RES:		return "RESET_MAINOBC_RES";
+		case EType::EXPAND_ANTENNA:			return "EXPAND_ANTENNA";
+		case EType::EXPAND_ANTENNA_RES:		return "EXPAND_ANTENNA_RES";
+		case EType::SET_FDIR:				return "SET_FDIR";
+		case EType::SET_FDIR_RES:			return "SET_FDIR_RES";
+		case EType::POWER:					return "POWER";
+		case EType::POWER_RES:				return "POWER_RES";
+		case EType::SEND_CW:				return "SEND_CW";
+		case EType::SEND_CW_RES:			return "SEND_CW_RES";
+		case EType::SEND_GMSK:				return "SEND_GMSK";
+		case EType::SEND_GMSD_RES:			return "SEND_GMSD_RES";
+		case EType::ABORT_DOWNLINK:			return "ABORT_DOWNLINK";
+		case EType::ABORT_DOWNLINK_RES:		return "ABORT_DOWNLINK_RES";
+		case EType::SET_CW_SPEED:			return "SET_CW_SPEED";
+		case EType::SET_CW_SPEED_RES:		return "SET_CW_SPEED_RES";
+		case EType::SELECT_TXOBC:			return "SELECT_TXOBC";
+		case EType::SELECT_TXOBC_RES:		return "SELECT_TXOBC_RES";
+		case EType::TX_RESET:				return "TX_RESET";
+		case EType::TX_RESET_RES:			return "TX_RESET_RES";
+		case EType::COMM_TEST:				return "COMM_TEST";
+		case EType::COMM_TEST_RES:			return "COMM_TEST_RES";
+		case EType::GET_HKDATA_FULL:		return "GET_HKDATA_FULL";
+		case EType::GET_HKDATA_FULL_RES:	return "GET_HKDATA_FULL_RES";
+		case EType::SET_CUR_TIME:			return "SET_CUR_TIME";
+		case EType::SET_CUR_TIME_RES:		return "SET_CUR_TIME_RES";
+		case EType::GET_HKDATA:				return "GET_HKDATA";
+		case EType::GET_HKDATA_RES:			return "GET_HKDATA_RES";
+		case EType::REQ_PING:				return "REQ_PING";
+		case EType::REQ_PING_RES:			return "REQ_PING_RES";
+		case EType::REQ_TEMPERATURE:		return "REQ_TEMPERATURE";
+		case EType::REQ_TEMPERATURE_RES:	return "REQ_TEMPERATURE_RES";
 		default: return "";
 	}
 }
